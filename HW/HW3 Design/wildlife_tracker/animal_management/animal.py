7426c09ae49754bba3d90a78ef60ab158567ff1b
Gerardo Vega <147276585+stargazerJerry2023@users.noreply.github.com>
2024-10-08 11:45:41 -0400

    Update animal.py

--- HW/HW3 Design/wildlife_tracker/animal_management/animal.py
+++ HW/HW3 Design/wildlife_tracker/animal_management/animal.py
@@ -1,5 +1,20 @@
 from typing import Any, Optional
 
 class Animal:
+    def __init__(self, animal_id: int, species: str, age: Optional[int] = None, health_status: Optional[str] = None):
+        self.animal_id = animal_id
+        self.sepcies = species
+        self.age = age
+        self.health_status = health_status
+    
 
-    pass
+    def get_animal_by_id(self, animal_id: int) -> Optional[Animal]
+        return self.animals.get(animal_id)
+
+    def get_animal_details(self, animal_id) -> dict[str, Any]:
+        return {
+            "animal_id": self.animal_id,
+            "species": self.species,
+            "age": self.age,
+            "health_status": self.health_status,
+        }
